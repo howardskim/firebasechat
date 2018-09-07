9a38f64a1b6710fe4bdb7b8c615a9c63a2a41c48
Howard Kim <howardkim820@gmail.com>
2018-09-07 16:14:32 -0700

    connected firebase

--- src/reducers/chat_reducer.js
+++ src/reducers/chat_reducer.js
@@ -1,11 +1,15 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-
+    log: {}
 };
 
 export default (state = DEFAULT_STATE, action) => {
     switch(action.type){
+        case types.UPDATE_CHAT_LOG:
+        return{
+            log: action.payload
+        }
         default:
         return state;
     }
